7504b67560dfcabc2a1eb85b5ae44dbfb9571ddd
wiker <linwiker@gmail.com>
2016-01-12 18:02:43 +0800

    修改索引从1开始

--- quotes.py
+++ quotes.py
@@ -7,5 +7,6 @@ import pandas as pd
 today = date.today()
 start = (today.year-1, today.month, today.day)
 quotes = quotes_historical_yahoo('AXP',start,today)
-df = pd.DataFrame(quotes)
-print df
+fields = ['date','open','close','high','low','colume']
+quotesdf = pd.DataFrame(quotes,index = range(1,len(quotes)+1),columns = fields)
+print quotesdf
