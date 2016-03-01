237f570507f3dc23985810c48adb5acdbcf834ec
wiker <linwiker@gmail.com>
2016-03-01 18:27:42 +0800

    从文本取账号密码以及次数

--- python3/auth.py
+++ python3/auth.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#用户认证，账号或者密码错误自动退出
+#从文本取得账号和密码进行用户认证，账号或者密码错误自动退出
 import getpass
 
+dict_temp={}
+flags = True
+f=open("auth.txt","r")
+for i in f.readlines():
+    data=i.strip().split(";")
+    dict_temp[data[0]]={"password":data[1],"count":data[2]}
+f.close()
 username=input("please input your username:")
-if username =='wiker':
-    for i in range(3):
-        passwd=getpass.getpass("please input your password:")
-        if passwd=='12345':
-            print('wellcome %s' % username)
-            break
+if username in dict_temp.keys():
+    flags=int(dict_temp[username]["count"])
+    while flags < 3:
+            passwd=getpass.getpass("please input your password:")
+            if passwd==dict_temp[username]["password"]:
+                print('wellcome %s' % username)
+                break
+            else:
+                flags += 1
     else:
-        print("用户名或者密码错误")
+         print("由于你输入错误次数超过三次，禁止再重新登录")
 else:
     for i in range(3):
         passwd=getpass.getpass("please input your password:")
