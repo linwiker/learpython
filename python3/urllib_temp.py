79134fd6155dcc23b3dcd2f2c00d3bede2fc801f
wiker <wiker@linwiker.com>
2016-03-28 21:58:45 +0800

    模拟使用iphone访问页面

---
+++ python3/urllib_temp.py
@@ -0,0 +1,15 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+from urllib import request
+
+def Geturl(url):
+    req = request.Request(url)
+    req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+    with request.urlopen(req) as f:
+        data = f.read()
+        print('Status:',f.status, f.reason)
+        for k, v in f.getheaders():
+            print('%s:%s' %(k,v))
+        print('Data:', data.decode('utf-8'))
+
+Geturl('https://www.douban.com')
