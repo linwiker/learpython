43fe27ec664d9d9d8ed35015b427d8db56ed8a89
wiker <linwiker@gmail.com>
2016-04-03 21:04:09 +0800

    栈举例：实现括号匹配示例

--- python3/data structure/stack.py
+++ python3/data structure/stack.py
@@ -21,8 +21,21 @@ class Stack:   #定义栈，两个方法push和pop
 
 if __name__ == '__main__':
     stack = Stack()
-    for i in range(10):
-        stack.push(i)
-
-    while stack.top:  #会看到反过来把我们插入的数值输出
-        print(stack.pop())
+#栈实现括号匹配
+    exp = '{a * [x/(x+y)]}'
+    for c in exp:
+        if c in '{[(': #如果是{[(则进行压栈
+            stack.push(c)
+        elif c in '}])': #如果是}])则与栈顶的数值进行比较
+            v = stack.top.value  #栈顶值
+            if c == '}' and v !=  '{':
+                raise Exception('{faild}')
+            if c == ']' and v !=  '[':
+                raise Exception('[faild]')
+            if c == ')' and v !=  '(':
+                raise Exception('(faild)')
+            stack.pop()
+    if stack.top is None:  #如果栈抛空了则输出OK，没有抛空输出异常
+        print('OK')
+    else:
+        raise Exception('faild')
