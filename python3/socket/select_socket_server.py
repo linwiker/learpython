ef9a67a948008b6bb0555f8de7a0eeadce93b030
wiker <linwiker@gmail.com>
2016-04-19 23:58:40 +0800

    添加备注信息

--- python3/socket/select_socket_server.py
+++ python3/socket/select_socket_server.py
@@ -16,16 +16,16 @@ inp = [sk1,]
 while True:
     readable_list, writeable_list, error_list = select.select(inp, [], inp, 1)
     for r in readable_list:
-        if sk1 == r:
+        if sk1 == r:  #当客户端第一次链接服务端的时候
             print('accept')
             request, address = r.accept()
             request.setblocking(0)
             inp.append(request)
             print(inp)
-        else:
+        else: #当客户端连接上服务端以后，再次发送数据时
             received = r.recv(1024)
-            if received:
+            if received: #正常接收客户端发送的数据时
                 print('received data:',received)
-            else:
+            else: #当客户端关闭程序时
                 inp.remove(r)
 sk1.close()
