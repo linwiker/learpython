293e586615ce1a276835be9f9c40ea6901c0a666
wiker <linwiker@gmail.com>
2016-04-28 00:39:16 +0800

    将进程定义为类

---
+++ python3/multiprocess_process_class.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+import multiprocessing
+import time
+
+class ClockProcess(multiprocessing.Process):
+    def __init__(self, interval):
+        super(ClockProcess, self).__init__(self)
+        self.interval = interval
+
+    def run(self):
+        n = 5
+        while n > 0:
+            print("the time is {0}".format(time.ctime()))
+            time.sleep(self.interval)
+            n -= 1
+
+if __name__ == '__main__':
+    p = ClockProcess(3)
+    p.start()
