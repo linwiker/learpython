734e24b1741665ba88e5baa6027eb355371d920c
wiker <linwiker@gmail.com>
2016-05-04 15:24:52 +0800

    add try...expect to multiprocess_queue.py

--- python3/multiprocess_queue.py
+++ python3/multiprocess_queue.py
@@ -3,17 +3,29 @@
 import multiprocessing
 
 def writer_proc(q):
-    q.put(1, block = False)
+    try:
+        q.put(1, block = False)
+    except:
+        pass
 
 def reader_proc(q):
-    print(q.get(block = False))
+    try:
+        print(q.get(block = False))
+    except:
+        pass
 
 if __name__ == '__main__':
     q = multiprocessing.Queue()
-    writer = multiprocessing.Process(target=writer_proc, args=(q,))
-    writer.start()
+    writer1 = multiprocessing.Process(target=writer_proc, args=(q,))
+    writer1.start()
+    writer2 = multiprocessing.Process(target=writer_proc, args=(q,))
+    writer2.start()
+    writer3 = multiprocessing.Process(target=writer_proc, args=(q,))
+    writer3.start()
     reader = multiprocessing.Process(target=reader_proc, args=(q,))
     reader.start()
 
-    writer.join()
+    writer1.join()
+    writer2.join()
+    writer3.join()
     reader.join()
