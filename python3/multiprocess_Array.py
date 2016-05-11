cdcd9b81eae908e23992ba2ff2a3a37ee05b799b
wiker <wiker@linwiker.com>
2016-05-11 21:03:43 +0800

    add mul array and manager

---
+++ python3/multiprocess_Array.py
@@ -0,0 +1,14 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+from multiprocessing import Process,Array
+
+temp = Array('i',[11,22,33,44])
+
+def Foo(i):
+    temp[i] = 100+i
+    for item in temp:
+        print(i,'------->',item)
+
+for i in range(2):
+    p = Process(target=Foo,args=(i,))
+    p.start()
