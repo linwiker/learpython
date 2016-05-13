fec28788ba25222e938f578e722c9e1bd614ab55
wiker <linwiker@gmail.com>
2016-05-13 18:13:26 +0800

    使用我们的模板实现redis的订阅

--- python3/redis_demo/redis_sub.py
+++ python3/redis_demo/redis_sub.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import redis
-r = redis.Redis()
-sub = r.pubsub()
-sub.subscribe('fm80')
-sub.parse_response()
+#import redis
+#r = redis.Redis()
+#sub = r.pubsub()
+#sub.subscribe('fm80')
+#sub.parse_response()
+#while True:
+#    z=sub.parse_response()
+#    print(z)
+import redisHelper
+
+obj = redisHelper.RedisHelper()
+redis_sub = obj.subscribe()
 while True:
-    z=sub.parse_response()
-    print(z)
+    msg = redis_sub.parse_response()
+    print(msg)
+
