7579ec09c6481c60afd060485c47526de332ba66
wiker <linwiker@gmail.com>
2016-05-13 17:56:11 +0800

    修改发布频道

--- python3/redis_demo/redisHelper.py
+++ python3/redis_demo/redisHelper.py
@@ -3,11 +3,11 @@
 import redis
 
 class RedisHelper:
-    pool = redis.ConnectionPool(host='10.99.56.124',port=6379,max_connections=1024)
     def __init__(self):
+        pool = redis.ConnectionPool(host='10.99.56.124',port=6379,max_connections=1024)
         self.__conn = redis.Redis(connection_pool=pool)
         self.chan_sub = 'fm90'
-        self.chan_pub = 'fm80'
+        self.chan_pub = 'fm90'
 
     def publish(self, msg):
         self.__conn.publish(self.chan_pub, msg)
