17c60b1bac74eea05859b508c395bfe0a6b6aec8
wiker <linwiker@gmail.com>
2016-05-14 20:43:46 +0800

    rabbitmq消费者模式使用acknoeledgment消息不丢失no-ack=False

--- python3/module_demo/rabbitmq_consumer.py
+++ python3/module_demo/rabbitmq_consumer.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #producer mode
 import pika
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
@@ -9,9 +10,12 @@ channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
     print("Received %r" % body)
+    #time.sleep(10)
+    print('OK')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(callback,
                         queue='hello',
-                        no_ack=True)
+                        no_ack=False)
 print('[*] Waiting for messages, To exit press CTRL+C')
 channel.start_consuming()
