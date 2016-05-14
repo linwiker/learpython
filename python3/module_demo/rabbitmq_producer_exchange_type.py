e8cbf87376e135856bb54fd74987f7c8d3aa70c4
wiker <linwiker@gmail.com>
2016-05-14 23:51:06 +0800

    rabbitmq Topic Exchange消费者和生产者模型实现

--- python3/module_demo/rabbitmq_producer_exchange_type.py
+++ python3/module_demo/rabbitmq_producer_exchange_type.py
@@ -5,10 +5,10 @@ import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-channel.exchange_declare(exchange='direct_logs',type='direct')
+channel.exchange_declare(exchange='topic_logs',type='topic')
 
-severity = sys.argv[1] if len(sys.argv) > 1 else "info"
+routing_key = sys.argv[1] if len(sys.argv) > 1 else "anonymous.info"
 message = ' '.join(sys.argv[2:]) or 'Hello World!'
-channel.basic_publish(exchange='direct_logs',routing_key=severity,body=message)
-print("Sent %r:%r" %(severity, message))
+channel.basic_publish(exchange='topic_logs',routing_key=routing_key,body=message)
+print("Sent %r:%r" %(routing_key, message))
 connection.close()
