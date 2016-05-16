238305096d853705cb68d3a3f1d6f0dd83145905
wiker <linwiker@gmail.com>
2016-05-16 17:14:03 +0800

    修改语法错误

--- python3/monitor/server/MonitorServer.py
+++ python3/monitor/server/MonitorServer.py
@@ -1,8 +1,8 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os,sys
-from core import main
-from conf import settings
+#from core import main
+#from conf import settings
 
 base_dir = os.path.dirname(os.path.dirname(__file__))
 print(base_dir)
--- python3/monitor/server/conf/settings.py
+++ python3/monitor/server/conf/settings.py
@@ -1,7 +1,7 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-RedisServer = 10.99.56.124
+RedisServer = '10.99.56.124'
 RedisPort = 6379
 RedisMaxconn = 1024
 RedisSubChannel = 'fm87.7'
