5696221e35c5b15779ab41a9c2c6b2968ee00d6b
wiker <linwiker@gmail.com>
2016-05-17 18:05:31 +0800

    定义handle收集客户端的监控数据写到redis里面

--- python3/monitor/server/core/main.py
+++ python3/monitor/server/core/main.py
@@ -4,6 +4,8 @@
 from .redisHelper import RedisHelper
 from .serialize import push_all_configs_into_redis
 from conf import hosts
+import json
+import time
 
 
 class MonitorServer():
@@ -20,4 +22,8 @@ class MonitorServer():
     def handle(self):
         chan_sub = self.r.subscribe()
         while True:
-            print(chan_sub.parse_response())
+            service_data=chan_sub.parse_response()
+            service_data = json.loads(service_data)[2]
+            service_data['timestap'] = time.time()
+            service_data_key = (ServerData::%s::%s %(service_data['host'],service_data['service']))
+            self.r.set(service_data_key,json.dumps(service_data['data']))
