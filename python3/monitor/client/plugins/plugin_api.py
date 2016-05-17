11961146198d3bec24a44398566a729ca72268c2
wiker <linwiker@gmail.com>
2016-05-17 16:47:13 +0800

    调用指定插件抓取资源的api

---
+++ python3/monitor/client/plugins/plugin_api.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+from plugins import cpu,memory
+
+def get_cpu_status():
+    return cpu.monitor()
+
+def get_mem_status():
+    return memory.monitor()
+
+if __name__ == '__main__':
+    print(get_cpu_status())
