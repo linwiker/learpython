337c9034104ae1652fdec347041dd6e4cc4e6bc1
wiker <linwiker@gmail.com>
2016-06-22 18:40:34 +0800

    添加schedule模块（实现watcher调度的）

---
+++ logscan/logscan/schedule.py
@@ -0,0 +1,28 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+import threading
+from os import path
+from .watch import Watcher
+from .match import Matcher
+
+class Schedule:
+
+    def __init__(self):
+        self.watchers = {}
+
+    def add_watcher(self, watcher):
+        #判断watcher线程是否启动，如果没启动的话，启动个watcher线程
+        if watcher.filename not in self.watchers.keys():
+            t = threading.Thread(target=watcher.start, name="Watcher-{0}".format(watcher.filename))
+            t.setDaemon(True)
+            t.start()
+            self.watchers[watcher.filename] = watcher
+
+    def remove_watcher(self, filename):
+        #移除watcher，如果key在watchers的字典里，则进行删除
+        key = path.abspath(filename)
+        if key in self.watchers.keys():
+            self.watchers[key].stop()
+            self.watchers.pop(key)
+
+
