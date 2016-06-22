a1dcd7b781021b826f1816a054e54f0075549c8e
wiker <linwiker@gmail.com>
2016-06-22 18:41:22 +0800

    app添加测试字段

--- logscan/app.py
+++ logscan/app.py
@@ -1,2 +1,15 @@
 #/usr/bin/env python
 # -*- coding: utf-8 -*-
+from logscan.match import Matcher
+from logscan.watch import Watcher
+from logscan.schedule import Schedule
+
+if __name__ == '__main__':
+    import sys
+    s = Schedule()
+    try:
+        s.add_watcher(Watcher(sys.argv[1], Matcher('#123#')))
+        s.add_watcher(Watcher(sys.argv[2], Matcher('#123#')))
+    except KeyboardInterrupt:
+        s.remove_watcher(sys.argv[1])
+        s.remove_watcher(sys.argv[2])
