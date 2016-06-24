961244b3385b5d1e88cd48a3d5b3d3c9775515a1
wiker <linwiker@gmail.com>
2016-06-24 16:01:37 +0800

    Message生成一个消息和Notification设定不同的放方式

--- logscan/logscan/check.py
+++ logscan/logscan/check.py
@@ -6,9 +6,50 @@ import threading
 def send_mail(user, message):
     pass
 
+class Message:
+    def __init__(self, user, name, path, count):
+        self.user = user
+        self.name = name
+        self.path = path
+        self.count = count
+
+class Notification:
+    def __init__(self):
+        self.message = None
+        self.__event = threading.Event()
+        self.__cond = threading.Condition()
+
+    def send_mail(self):
+        #等待notify方法来通知然后进行消息的发送
+        while not self.__event.is_set():
+            with self.__cond:
+                self.__cond.wait()
+                #TODO
+
+    def send_sms(self):
+        while not self.__event.is_set():
+            with self.__cond:
+                self.__cond.wait()
+                #TODO
+
+    def notify(self, message):
+        #使用self__cond来生产消息，然后消息生产完以后通知指定的方式进行消息的发送
+        with self.__cond:
+            self.message = message
+            self.__cond.notify_all()
+
+    def start(self):
+        mail = threading.Thread(target=send_mail, name='send_mail')
+        mail.start()
+        sms = threading.Thread(target=send_sms, name='send_sms')
+        sms.start()
+
+
+
+
 class Checker:
 
-    def __init__(self, name, expr, path, interval, threshold, users, counter):
+    def __init__(self, name, expr, path, interval, threshold, users, counter, notification):
         #使用config.json传入的值
         self.name = name
         self.expr = expr
@@ -18,8 +59,9 @@ class Checker:
         self.users = users
         self.counter = counter
         self.__event = threading.Event()
+        self.notification = notification
 
-    def check(self):
+    def start(self):
         #判断是否到达阈值，触发规则则调用报警
         while not self.__event.is_set():
             self.__event.wait(self.interval * 60) #使用wait代替time.sleep的原因是如果我们想退出的话，但是线程还处在sleep
@@ -30,11 +72,9 @@ class Checker:
                 if count < self.threshold[1] or self.threshold < 0:
                     self.notify('{0} matched {1} times in {2}min'.format(self.name, count, self.interval))
 
-    def notify(self, message):
+    def notify(self, count):
         for user in self.users:
-            t = threading.Thread(target=send_mail, args=(user, message), name='mail_send-{0}'.format(user))
-            t.start()
-            t.join()
+            message = Message(user, self.name, self.path, count)
 
     def stop(self):
         self.__event.set()
