fdd58ece52e63d59b2b0becdac5b80cd7309ea45
wiker <linwiker@gmail.com>
2016-06-24 11:18:28 +0800

    修改bug，以及删除测试代码

--- logscan/logscan/count.py
+++ logscan/logscan/count.py
@@ -19,7 +19,7 @@ class Counter:
 
     def get(self, name):
         with self.lock:
-            self.db.get[name, 0]
+            self.db.get(name, 0)
 
     def clean(self, name):
         with self.lock:
@@ -29,27 +29,3 @@ class Counter:
         with self.lock:
             if not self.stoped:
                 self.db.close()
-
-
-if __name__ == '__main__':
-    import logging
-    import time
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s[line: %(lineno)d] %(levelname)s %(message)s [%(threadName)s]',
-                    datefmt='%a, %d %b %Y %H:%M:%S')
-    counter = Counter()
-
-    def inc(counter):
-        for x in range(10):
-            counter.inc()
-            time.sleep(0.2)
-    def get(counter):
-        time.sleep(0.2)
-        logging.debug("Current value is {0}".format(counter.val))
-
-    inc1 = threading.Thread(target=inc, name='inc1', args=(counter,))
-    inc2 = threading.Thread(target=inc, name='inc2', args=(counter,))
-    g = threading.Thread(target=get, name='get', args=(counter,))
-
-    inc1.start()
-    inc2.start()
-    g.start()
