9d9f0700a531093f5031e463f21c711f852c811e
wiker <linwiker@gmail.com>
2016-06-24 11:23:33 +0800

    添加注释

--- logscan/logscan/count.py
+++ logscan/logscan/count.py
@@ -13,15 +13,18 @@ class Counter:
         self.stoped = False
 
     def inc(self, name):
+        #对规则的名称（name）进行计数
         with self.lock:
             self.db[name] = self.db.get(name, 0) + 1
 
 
     def get(self, name):
+        #获取规则的数量，如果没有的话则报0
         with self.lock:
             self.db.get(name, 0)
 
     def clean(self, name):
+        #清除规则的计数
         with self.lock:
             self.db.pop(name)
 
