723642c3877c7ebb9874143a7a94bee034e7c87d
wiker <linwiker@gmail.com>
2016-06-30 11:39:20 +0800

    使用队列来做MatcherChain

--- logscan/logscan/watch.py
+++ logscan/logscan/watch.py
@@ -3,17 +3,17 @@
 import os
 import sys
 import threading
+from queue import Queue
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
-from .match import Matcher
+from .match import MatcherChain
 
 class Watcher(FileSystemEventHandler):
 
-    def __init__(self, filename, checker):
+    def __init__(self, filename, counter):
         self.filename = os.path.abspath(filename)
-        self.matcher = Matcher(checker.name, checker.expr)
-        self.checker = checker
-        self.counter = None
+        self.queue =  Queue()
+        self.matcherchain = MatcherChain(queue=self.queue, counter)
         self.observer = Observer()
         self.fd = None
         self.offset = 0
@@ -23,14 +23,13 @@ class Watcher(FileSystemEventHandler):
             self.offset = os.path.getsize(self.filename)
 
     def start(self):
-        t = threading.Thread(target=self.checker, name='check-{0}'.format(self.checker.name))
-        t.start()
+        self.matchers.start()
         self.observer.schedule(self, os.path.dirname(self.filename), recursive=False)
         self.observer.start()
         self.observer.join()
 
     def stop(self):
-        self.checker.stop()
+        self.matcherchain.stop()
         self.observer.stop()
         if self.fd is not None and not self.fd.closed:
             self.fd.close()
@@ -40,9 +39,7 @@ class Watcher(FileSystemEventHandler):
             self.fd.seek(self.offset, 0)
             for line in self.fd:
                 line = line.rstrip('\n')
-                if self.matcher.match(line):
-                    if self.counter is not None:
-                        self.counter.inc(self.matcher.name)
+                self.queue.put(line)
             self.offset = self.fd.tell()
 
     def on_deleted(self, event):
