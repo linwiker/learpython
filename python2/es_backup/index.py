99d264188ac8085ecfe8bab2a1185608aba27deb
wiker <linwiker@gmail.com>
2016-12-13 10:51:05 +0800

    查看es指定站点pv量

--- python2/es_backup/index.py
+++ python2/es_backup/index.py
@@ -43,14 +43,14 @@ class Es():
         s=self.es.snapshot.create(repository=repos,snapshot=snapname,body=self.q, wait_for_completion=True)
         print s
 
-    self.es.
 
 
 
 if __name__ == '__main__':
     es=Es("172.17.43.122:9200")
-    f=es.snap("weekly","api_test")
- #   f=es.get_pv("http_host:api.invest.ppdai.com", "api-nginx-access-2016.11.06")
+    # f=es.snap("weekly","api_test")
+    f=es.get_pv("http_host:api.invest.ppdai.com", "api-nginx-access-2016.11.06")
+    print f
 #
 
 
